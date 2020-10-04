2356e2535440c838b4637f7f3208172f75af2c32
CorpNewt <12772521+corpnewt@users.noreply.github.com>
2020-10-04 17:33:54 -0500

    Update plist.py, downloader.py from pymodules

--- Scripts/downloader.py
+++ Scripts/downloader.py
@@ -1,4 +1,5 @@
-import sys, os, time, ssl
+import sys, os, time, ssl, gzip
+from io import BytesIO
 # Python-aware urllib stuff
 if sys.version_info >= (3, 0):
     from urllib.request import urlopen, Request
@@ -11,6 +12,18 @@ class Downloader:
 
     def __init__(self,**kwargs):
         self.ua = kwargs.get("useragent",{"User-Agent":"Mozilla"})
+
+        # Provide reasonable default logic to workaround macOS CA file handling 
+        cafile = ssl.get_default_verify_paths().openssl_cafile
+        try:
+            # If default OpenSSL CA file does not exist, use that from certifi
+            if not os.path.exists(cafile):
+                import certifi
+                cafile = certifi.where()
+            self.ssl_context = ssl.create_default_context(cafile=cafile)
+        except:
+            # None of the above worked, disable certificate verification for now
+            self.ssl_context = ssl._create_unverified_context()
         return
 
     def _decode(self, value, encoding="utf-8", errors="ignore"):
@@ -24,40 +37,45 @@ class Downloader:
         headers = self.ua if headers == None else headers
         # Wrap up the try/except block so we don't have to do this for each function
         try:
-            response = urlopen(Request(url, headers=headers))
+            response = urlopen(Request(url, headers=headers), context=self.ssl_context)
         except Exception as e:
-            if sys.version_info >= (3, 0) or not (isinstance(e, urllib2.URLError) and "CERTIFICATE_VERIFY_FAILED" in str(e)):
-                # Either py3, or not the right error for this "fix"
-                return None
-            # Py2 and a Cert verify error - let's set the unverified context
-            context = ssl._create_unverified_context()
-            try:
-                response = urlopen(Request(url, headers=headers), context=context)
-            except:
-                # No fixing this - bail
-                return None
+            # No fixing this - bail
+            return None
         return response
 
-    def get_size(self, size, suff=None):
+    def get_size(self, size, suffix=None, use_1024=False, round_to=2, strip_zeroes=False):
+        # size is the number of bytes
+        # suffix is the target suffix to locate (B, KB, MB, etc) - if found
+        # use_2014 denotes whether or not we display in MiB vs MB
+        # round_to is the number of dedimal points to round our result to (0-15)
+        # strip_zeroes denotes whether we strip out zeroes 
+
+        # Failsafe in case our size is unknown
         if size == -1:
             return "Unknown"
-        ext = ["B","KB","MB","GB","TB","PB"]
+        # Get our suffixes based on use_1024
+        ext = ["B","KiB","MiB","GiB","TiB","PiB"] if use_1024 else ["B","KB","MB","GB","TB","PB"]
+        div = 1024 if use_1024 else 1000
         s = float(size)
-        s_dict = {}
-        # Iterate the ext list, and divide by 1000 each time
+        s_dict = {} # Initialize our dict
+        # Iterate the ext list, and divide by 1000 or 1024 each time to setup the dict {ext:val}
         for e in ext:
             s_dict[e] = s
-            s /= 1000
-        if suff and suff.upper() in s_dict:
-            # We supplied the suffix - use it \o/
-            bval = round(s_dict[suff.upper()], 2)
-            biggest = suff.upper()
-        else:
-            # Get the maximum >= 1 type
-            biggest = next((x for x in ext[::-1] if s_dict[x] >= 1), "B")
-            # Round to 2 decimal places
-            bval = round(s_dict[biggest], 2)
-        return "{:,.2f} {}".format(bval, biggest)
+            s /= div
+        # Get our suffix if provided - will be set to None if not found, or if started as None
+        suffix = next((x for x in ext if x.lower() == suffix.lower()),None) if suffix else suffix
+        # Get the largest value that's still over 1
+        biggest = suffix if suffix else next((x for x in ext[::-1] if s_dict[x] >= 1), "B")
+        # Determine our rounding approach - first make sure it's an int; default to 2 on error
+        try:round_to=int(round_to)
+        except:round_to=2
+        round_to = 0 if round_to < 0 else 15 if round_to > 15 else round_to # Ensure it's between 0 and 15
+        bval = round(s_dict[biggest], round_to)
+        # Split our number based on decimal points
+        a,b = str(bval).split(".")
+        # Check if we need to strip or pad zeroes
+        b = b.rstrip("0") if strip_zeroes else b.ljust(round_to,"0") if round_to > 0 else ""
+        return "{:,}{} {}".format(int(a),"" if not b else "."+b,biggest)
 
     def _progress_hook(self, response, bytes_so_far, total_size):
         if total_size > 0:
@@ -77,7 +95,7 @@ class Downloader:
         response = self.open_url(url, headers)
         if not response:
             return None
-        CHUNK = 16 * 1024
+        CHUNK = 1024 * 1024
         bytes_so_far = 0
         try:
             total_size = int(response.headers['Content-Length'])
@@ -98,7 +116,7 @@ class Downloader:
         response = self.open_url(url, headers)
         if not response:
             return None
-        CHUNK = 16 * 1024
+        CHUNK = 1024 * 1024
         bytes_so_far = 0
         try:
             total_size = int(response.headers['Content-Length'])
@@ -113,13 +131,17 @@ class Downloader:
             if not chunk:
                 break
             chunk_so_far += chunk
+        if response.headers.get('Content-Encoding') == "gzip":
+            fileobj = BytesIO(chunk_so_far)
+            file = gzip.GzipFile(fileobj=fileobj)
+            return file.read()
         return chunk_so_far
 
     def stream_to_file(self, url, file, progress = True, headers = None):
         response = self.open_url(url, headers)
         if not response:
             return None
-        CHUNK = 16 * 1024
+        CHUNK = 1024 * 1024
         bytes_so_far = 0
         try:
             total_size = int(response.headers['Content-Length'])
